d589c455039d5fc06a8394b62a7eb5d48614a636
Andrius <andriuscere@gmail.com>
2018-01-31 14:07:39 +0200

    Uzduotis0304

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Main objektas = new Main();
+    }
+    public Main(){
+        float suma=0;
+        float skaicius=0;
+        do{
+            skaicius= duokGarantuotaiSkaiciu();
+            suma += skaicius;
+        } while (skaicius!=0);
+        System.out.println("Suma: "+suma);
+    }
+    public float duokGarantuotaiSkaiciu(){
+        float rezultatas=0;
+        Scanner sc = new Scanner(System.in);
+        boolean arPavyko = false;
+        while (arPavyko==false){
+            System.out.println("Iveskite skaicius, kol bus 0");
+            try {
+                rezultatas = sc.nextFloat();
+                arPavyko = true;
+            }catch (Exception klaida) {
+            System.out.println("Ivestas tekstas yra ne skaicius");
+            sc.next();
+        }
+        }
+        return rezultatas;
     }
 }
+
