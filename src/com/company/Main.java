d124756149ce31d71ece2717f8b5a6f7d90cb65d
Andrius <andriuscere@gmail.com>
2018-01-31 14:10:49 +0200

    Uzduotis0304

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,37 @@
 package com.company;
-import java.util.Scanner;
-public class Main {
 
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Main {
+    public static final int EXIT_CODE = 0;
+
+    public static void main(String[] aEXIT_CODErgs) {
         Main objektas = new Main();
     }
-    public Main(){
-        float suma=0;
-        float skaicius=0;
-        do{
-            skaicius= duokGarantuotaiSkaiciu();
+
+    public Main() {
+        float suma = 0;
+        float skaicius = 0;
+        do {
+            skaicius = duokGarantuotaiSkaiciu();
             suma += skaicius;
-        } while (skaicius!=0);
-        System.out.println("Suma: "+suma);
+        } while (skaicius != EXIT_CODE);
+        System.out.println("Suma: " + suma);
     }
-    public float duokGarantuotaiSkaiciu(){
-        float rezultatas=0;
+
+    public float duokGarantuotaiSkaiciu() {
+        float rezultatas = 0;
         Scanner sc = new Scanner(System.in);
         boolean arPavyko = false;
-        while (arPavyko==false){
+        while (arPavyko == false) {
             System.out.println("Iveskite skaicius, kol bus 0");
             try {
                 rezultatas = sc.nextFloat();
                 arPavyko = true;
-            }catch (Exception klaida) {
-            System.out.println("Ivestas tekstas yra ne skaicius");
-            sc.next();
-        }
+            } catch (Exception klaida) {
+                System.out.println("Ivestas tekstas yra ne skaicius");
+                sc.next();
+            }
         }
         return rezultatas;
     }
